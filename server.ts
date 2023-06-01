d976f181b66d1465ef11bb644a8806f17cc37649
zachuri <punsalang.zachary@gmail.com>
2023-06-01 12:23:05 -0700

    response: able to get the languages from the repository with their count

--- server.ts
+++ server.ts
@@ -29,26 +29,35 @@ app.get("/user-stats", async (req: Request, res: Response) => {
 		let totalStargazers = 0;
 		let totalForkCount = 0;
 		let totalSize = 0;
+		let languages: string[] = [];
 		repositories.forEach(
 			(repo: {
 				size: number;
 				forks_count: number;
 				stargazers_count: number;
+				language: string;
 			}) => {
 				totalSize += repo.size;
 				totalForkCount += repo.forks_count;
 				totalStargazers += repo.stargazers_count;
+				const repoLanguages = repo.language;
+				languages = languages.concat(repoLanguages);
 			}
 		);
 
+		// Obtain Average Size
 		const averageSize = calculateAverageSize(totalSize, totalCount);
+
+		// Obtain language count
+		console.log(languages);
+		const languageCounts = calculateLanguageCounts(languages);
 
 		const finalResponse = {
 			totalCount: totalCount,
 			totalStargazers: totalStargazers /* calculated total stargazers */,
 			totalForkCount: totalForkCount /* calculated total fork count */,
 			averageSize: averageSize /* calculated average size */,
-			languages: "" /* calculated language counts */,
+			languages: languageCounts /* calculated language counts */,
 		};
 
 		res.status(200).send(finalResponse);
@@ -72,6 +81,28 @@ function calculateAverageSize(totalSize: number, totalCount: number) {
 	} else {
 		return `${(averageSizeBytes / 1048576).toFixed(2)} GB`;
 	}
+}
+
+function calculateLanguageCounts(languages: string[]) {
+	const languageCounts: { [key: string]: number } = {};
+
+	languages.forEach(language => {
+		if (languageCounts[language]) {
+			languageCounts[language]++;
+		} else {
+			languageCounts[language] = 1;
+		}
+	});
+
+	// Convert language counts to array of objects
+	const languageCountsArray = Object.entries(languageCounts).map(
+		([language, count]) => ({ language, count })
+	);
+
+	// Sort languages by count in descending order
+	const sortedLanguages = languageCountsArray.sort((a, b) => b.count - a.count);
+
+	return sortedLanguages;
 }
 
 // // Fetch user repostiory data
