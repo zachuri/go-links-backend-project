3cf6bf73034854748ade1ab607a22ff7af2fcde5
zachuri <punsalang.zachary@gmail.com>
2023-06-01 10:34:01 -0700

    response: was able to get a response of user data

--- server.ts
+++ server.ts
@@ -7,9 +7,23 @@ const port = 3000;
 
 app.get("/user-stats", async (req: Request, res: Response) => {
 	try {
-		const { username } = req.query;
+		const { username, forked } = req.query;
 
-		res.status(200).send(username);
+		// We need a username input
+		if (!username) {
+			res.status(418).send({ message: "We need a username input!!" });
+		}
+
+		// Fetch user repostiory data
+		const apiUrl = `https://api.github.com/users/${username}/repos`;
+		const headers = {
+			Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+			"Content-Type": "application/json",
+		};
+
+		const response = await axios.get(apiUrl, { headers });
+
+		res.status(200).send(response.data);
 	} catch (error) {
 		console.log("Error: ", error);
 		res.status(418).send({ message: "error" });
