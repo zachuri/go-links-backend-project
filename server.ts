0172edf7ff08c854b9249d9ca8fccb6628e210c6
zachuri <punsalang.zachary@gmail.com>
2023-06-01 10:25:31 -0700

    request: able to get query input

--- server.ts
+++ server.ts
@@ -1,12 +1,38 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import axios from "axios";
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/user-stats", async (req: Request, res: Response) => {
+	try {
+		const { username } = req.query;
+
+		res.status(200).send(username);
+	} catch (error) {
+		console.log("Error: ", error);
+		res.status(418).send({ message: "error" });
+	}
 });
 
+// // app.get("/", (req, res) => {
+// app.get("/user-stats", async (req: Request, res: Response) => {
+// 	try {
+// 		const { username, forked } = req.query;
+// 		// const { username } = req.body;
+
+// 		// if (!username) {
+// 		// 	res.status(418).send({ message: "We need a username input!!" });
+// 		// }
+
+// 		// res.status(200).send("Hello, world!");
+// 		res.status(200).send(username);
+// 	} catch (error) {
+// 		res.status(418).send({ message: "We need a username input!!" });
+// 	}
+// });
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+	console.log(`Server is running on port ${port}.`);
 });
